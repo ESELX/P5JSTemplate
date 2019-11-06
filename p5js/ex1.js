82996b5c1b42d29487e9d300f03cf23e5528d5ae
André Rocha <steam228@gmail.com>
2019-11-06 17:33:36 +0000

    p5js ajustes2

--- p5js/ex1.js
+++ p5js/ex1.js
@@ -1,17 +1,20 @@
+//Variáveis tipo let nomeDaVar;
 
+function setup() { //código que só corre uma vez
 
-function setup() {
+  ///Não Apagar para Baixo até <<<
   var canvasDiv = document.getElementById('ex1');
   var width = canvasDiv.offsetWidth;
   var sketchCanvas = createCanvas(width,width*.75);
   console.log(sketchCanvas);
   sketchCanvas.parent('ex1');
+  ///<<< Não fica a faltar createCanvas
 
 
 }
 
-function draw() {
-  background(220);
+function draw() { //código que corre em loop
+  background(220); //podem apagar esta linha!
 
 
 }
