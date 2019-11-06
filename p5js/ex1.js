9c8fd6047d12c2de37a21e53c73fc1de9466cf2f
André Rocha <steam228@gmail.com>
2019-11-06 12:55:15 +0000

    template simples

--- p5js/ex1.js
+++ p5js/ex1.js
@@ -1,30 +1,19 @@
 
-
+let posX, posY;
+let velX, velY;
 
 function setup() {
   var canvasDiv = document.getElementById('ex1');
   var width = canvasDiv.offsetWidth;
-  var sketchCanvas = createCanvas(width,300);
-  console.log(sketchCanvas1);
+  var sketchCanvas = createCanvas(width,width*.75);
+  console.log(sketchCanvas);
   sketchCanvas.parent('ex1');
+
+
 }
 
 function draw() {
-  background(200);
+  background(220);
 
-  fill(255, 191, 15);
-  stroke(51);
-  strokeWeight(4);
-  rect(20,20,70,70);
 
-  noFill();
-  stroke(51);
-  strokeWeight(2);
-
-  ellipse(90,90,70,70);
-
-  triangle((width/2)-30, height/2+30, (width/2)+30, height/2+30, width/2, (height/2)-30);
-
-  bezier(20, 20, width/2, height, width/2, height, width-20, 20)
-  bezier(20, 20, mouseX, mouseY, mouseX, mouseY, width-20, 20)
 }
